c6050610e4a5cc751e858ec824104239ecea96aa
Ravi <raviramchand8@gmail.com>
2021-10-25 16:17:30 -0400

    added comments to AppointmentController.java

--- src/main/java/ca/sheridan/ramchrav/appointmentcreator/controller/AppointmentController.java
+++ src/main/java/ca/sheridan/ramchrav/appointmentcreator/controller/AppointmentController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+//Makes the class into a Controller
 @Controller
 public class AppointmentController {
     @Autowired
@@ -15,7 +16,9 @@ public class AppointmentController {
 
     @GetMapping("/")
     public String getIndex(Model model, @ModelAttribute Appointment appointment){
+        //Send database records to index.html
         model.addAttribute("aList", da.getAppointment());
+        //Send empty object to index.html
         model.addAttribute("appointment", new Appointment());
         return "index";
     }
