abf4f5207f93811799e6cb8d3f435f759ee2a2dc
Ravi <raviramchand8@gmail.com>
2021-10-25 16:05:52 -0400

    added dummy records

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO appointment(firstName, email, aDate, aTime)
+VALUES ('Frank', 'frank@frank.com', '2020-09-01', '12:00:00'),
+       ('Hao', 'hao@gmail.com', '2020-10-02', '08:15:00'),
+       ('Sue', 'sue@yahoo.ca', '2020-11-03', '14:30:00'),
+       ('Jaspreet', 'jaspreet@outlook.com', '2020-12-04', '10:15:00');
