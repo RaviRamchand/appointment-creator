4a05617a9cce8e26004374afa7e793749de2ae71
Ravi <raviramchand8@gmail.com>
2021-10-25 16:03:31 -0400

    main method

---
+++ src/main/java/ca/sheridan/ramchrav/appointmentcreator/AppointmentCreatorApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridan.ramchrav.appointmentcreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentCreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppointmentCreatorApplication.class, args);
+    }
+
+}
