01ad28090b5c9c61c181bb40ccd04a57446400c5
Ravi <raviramchand8@gmail.com>
2021-10-25 16:02:02 -0400

    added databaseaccess file with select statement

---
+++ src/main/java/ca/sheridan/ramchrav/appointmentcreator/database/DatabaseAccess.java
@@ -0,0 +1,22 @@
+package ca.sheridan.ramchrav.appointmentcreator.database;
+
+import ca.sheridan.ramchrav.appointmentcreator.beans.Appointment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class DatabaseAccess {
+    @Autowired
+    protected NamedParameterJdbcTemplate jdbc;
+
+    public List<Appointment> getAppointment(){
+        MapSqlParameterSource namedParameter = new MapSqlParameterSource();
+        String query = "SELECT * FROM appointment";
+        return jdbc.query(query, namedParameter, new BeanPropertyRowMapper<Appointment>(Appointment.class));
+    }
+}
